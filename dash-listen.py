017cf8ed015424ec8768b0712e59c47596df1e8e
Jordan Caudill <Jordan@Jordans-Air.home>
2016-03-09 23:12:21 -0600

    esrdfg

--- dash-listen.py
+++ dash-listen.py
@@ -1,11 +1,11 @@
 import pexpect
 from scapy.all import *
-inGame = false #test
+inGame = false
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
       if pkt[ARP].hwsrc == 'a0:02:dc:9b:74:3d':
-        if inGame == false;
+        if inGame == false:
           print "Connecting to dash button, then running script"
           game = pexpect.spawn('sudo moonlight stream 192.168.1.11 -1080 -localaudio')
           inGame = true
