272d0c1cbd06b7daa777221093c084aca5b4fba4
Jordan Caudill <Jordan@Jordans-Air.home>
2016-03-09 22:22:10 -0600

    aesrdfg

--- dash-listen.py
+++ dash-listen.py
@@ -10,7 +10,4 @@ def arp_display(pkt):
       if pkt[ARP].hwsrc == 'a0:02:dc:9b:74:3d':
         print "Connecting to dash button, then running script"
         os.system("sudo moonlight stream 192.168.1.11 -1080 -localaudio"
-      else:
-        print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
-
 print sniff(prn=arp_display, filter="arp", store=0, count=10)
