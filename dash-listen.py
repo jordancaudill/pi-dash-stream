0d208b03933934e349c8a27e07919916d97f0e62
Jordan Caudill <Jordan@Jordans-Air.home>
2016-03-09 22:39:13 -0600

    adfsgfbc

--- dash-listen.py
+++ dash-listen.py
@@ -1,13 +1,9 @@
-import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-
 import os
-
 from scapy.all import *
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-      if pkt[ARP].hwsrc == 'a0:02:dc:9b:74:3d':
+      if pkt[ARP].hwsrc == 'a0:02:dc:9b:74:3d': # Huggies
         print "Connecting to dash button, then running script"
         os.system("sudo moonlight stream 192.168.1.11 -1080 -localaudio")
       else:
