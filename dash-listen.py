978022b3d3504d908ef4b06d4ccd434b22867e63
Jordan Caudill <Jordan@Jordans-Air.home>
2016-03-09 22:17:40 -0600

    blah

--- dash-listen.py
+++ dash-listen.py
@@ -1,16 +1,15 @@
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-import time
+import os
 
 from scapy.all import *
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
       if pkt[ARP].hwsrc == 'a0:02:dc:9b:74:3d':
-        #import textMe
-        time.sleep(15)
-        #import callMe
+        print "Connecting to dash button, then running script"
+        os.system("sudo moonlight stream 192.168.1.11 -1080 -localaudio"
       else:
         print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
