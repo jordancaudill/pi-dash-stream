877712387ea310bf47efb9d8a9502f207b1bab44
Jordan Caudill <jacaudill74@gmail.com>
2016-09-25 16:34:31 -0500

    awfs

--- dash-listen.py
+++ dash-listen.py
@@ -4,6 +4,7 @@ import os
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
     if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+    print "detected an ARP probe"
       if pkt[ARP].hwsrc == 'a0:02:dc:9b:74:3d':
           print "Connecting to dash button, then running script"
           os.system("sudo moonlight stream 192.168.1.6 -1080 -localaudio")
