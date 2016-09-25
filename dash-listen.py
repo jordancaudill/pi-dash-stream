5a8386e4a9bbc5008c27b21631fe3dca6b181902
Jordan Caudill <jacaudill74@gmail.com>
2016-09-25 16:40:00 -0500

    just stream

--- dash-listen.py
+++ dash-listen.py
@@ -1,14 +1,3 @@
-from scapy.all import *
 import os
-
-def arp_display(pkt):
-  if pkt[ARP].op == 1: #who-has (request)
-    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-      print "detected an ARP probe"
-      if pkt[ARP].hwsrc == 'a0:02:dc:9b:74:3d':
-          print "Connecting to dash button, then running script"
-          os.system("sudo moonlight stream 192.168.1.6 -1080 -localaudio")
-      else:
-        print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
-
-print sniff(prn=arp_display, filter="arp", store=0, count=0)
+print "Let's play some games"
+os.system("sudo moonlight stream 192.168.1.6 -1080 -localaudio")
